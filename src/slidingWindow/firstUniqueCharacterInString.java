db97d4ae1336b641b883f57b5d0ae1082cb6b63d
om kaldate <omkaldate23@gmail.com>
2024-03-23 23:29:16 +0530

    slidingWindow

---
+++ src/slidingWindow/firstUniqueCharacterInString.java
@@ -0,0 +1,23 @@
+package slidingWindow;
+
+public class firstUniqueCharacterInString {
+    public static void main(String[] args){
+        String s ="eeetcode";
+        System.out.println(firstUniqChar(s));
+    }
+
+    public static int firstUniqChar(String s) {
+
+        if(s.length()==1) return 0;
+        int freq[] = new int[26];
+        char t[] = s.toCharArray();
+
+        for(char i:t){
+            freq[i-'a']++;
+        }
+        for(char i:t){
+            if(freq[i-'a']==1) return s.indexOf(i);
+        }
+        return -1;
+    }
+}
