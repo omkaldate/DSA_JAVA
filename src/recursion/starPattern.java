eecfaa85543456cac3459dd7cfc98a15e65dee7e
om kaldate <omkaldate23@gmail.com>
2024-03-28 21:34:12 +0530

    star pattern

---
+++ src/recursion/starPattern.java
@@ -0,0 +1,37 @@
+package recursion;
+
+public class starPattern {
+    public static void main(String[] args){
+   // triangle(4,0);
+        triangle1(4,0);
+    }
+
+//    static void triangle(int r , int c){
+//        if(r==0){
+//            return;
+//        }
+//        if(c<r){
+//            System.out.print("*");
+//            triangle(r,c+1);
+//        }
+//        else{
+//            System.out.println();
+//            triangle(r-1,0);
+//        }
+//    }
+
+
+    static void triangle1(int r , int c){
+        if(r==0){
+            return;
+        }
+        if(c<r){
+            triangle1(r,c+ 1);
+            System.out.print("*");
+        }
+        else{
+            triangle1(r-1,0);
+            System.out.println();
+        }
+    }
+}
