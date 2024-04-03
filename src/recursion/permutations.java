517b134a04a9856310ae19c68ee80cc0c310f996
om kaldate <omkaldate23@gmail.com>
2024-04-03 13:24:01 +0530

    ds reursion

--- src/recursion/permutations.java
+++ src/recursion/permutations.java
@@ -5,7 +5,8 @@ import java.util.ArrayList;
 public class permutations {
     public static void main(String[] args){
        //permutations("" , "abc");
-       System.out.println(permutations("" , "abc"));
+       //System.out.println(permutations("" , "abc"));
+        System.out.println(permutationsCount("" , "abc"));
     }
 
 //    public static void permutations(String p , String up){
@@ -22,19 +23,36 @@ public class permutations {
 //        }
 //    }
 
-    public static ArrayList<String> permutations(String p , String up){
+//    public static ArrayList<String> permutations(String p , String up){
+//        if(up.isEmpty()){
+//            ArrayList<String> list = new ArrayList<>();
+//            list.add(p);
+//            return list;
+//        }
+//        ArrayList<String> ans = new ArrayList<>();
+//        char ch = up.charAt(0);
+//        for(int i=0; i<=p.length(); i++){
+//            String f = p.substring(0,i);
+//            String s = p.substring(i,p.length());
+//            ans.addAll(permutations(f+ch+s , up.substring(1)));
+//        }
+//        return ans;
+//    }
+
+
+    public static int permutationsCount(String p , String up){
         if(up.isEmpty()){
-            ArrayList<String> list = new ArrayList<>();
-            list.add(p);
-            return list;
+            return 1;
         }
-        ArrayList<String> ans = new ArrayList<>();
+        int count =0;
         char ch = up.charAt(0);
         for(int i=0; i<=p.length(); i++){
             String f = p.substring(0,i);
             String s = p.substring(i,p.length());
-            ans.addAll(permutations(f+ch+s , up.substring(1)));
+           count = count + permutationsCount(f+ch+s , up.substring(1));
         }
-        return ans;
+        return count;
     }
+
+
 }
