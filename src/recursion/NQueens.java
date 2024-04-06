532b73bb0586519a864c43e7f05c9c7740d4af03
om kaldate <omkaldate23@gmail.com>
2024-04-07 00:12:54 +0530

    ds

--- src/recursion/NQueens.java
+++ src/recursion/NQueens.java
@@ -4,6 +4,7 @@ public class NQueens {
     public static void main(String[] args){
         int n =4;
         boolean[][]board = new boolean[n][n];
+        //System.out.println(board[0][2]);
         System.out.println(queens(board , 0));
     }
 
