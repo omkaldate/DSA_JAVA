e18d38a03304b9bd35a438ac2f397745fc94e689
om kaldate <omkaldate23@gmail.com>
2024-04-18 21:09:08 +0530

    ds

---
+++ src/oopsCollage_practicals/theKthFactorOfN.java
@@ -0,0 +1,17 @@
+package oopsCollage_practicals;
+
+//Input: n = 12, k = 3
+//Output: 3
+//Explanation: Factors list is [1, 2, 3, 4, 6, 12], the 3rd factor is 3.
+
+public class theKthFactorOfN {
+    public static void main(String[] args){
+       int n = 12;
+       int k = 3;
+
+    }
+
+    static void factor(int n){
+        
+    }
+}
