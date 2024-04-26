481451d77166f805117ee46370dba200aeae2186
om kaldate <omkaldate23@gmail.com>
2024-04-27 01:03:45 +0530

    ds

--- src/string/reverseOnlyLetters.java
+++ src/string/reverseOnlyLetters.java
@@ -2,7 +2,7 @@ package string;
 
 public class reverseOnlyLetters {
     public static void main(String[] args) {
-        String s = "ab-cd";
+        String s = "7-89]";
         char[] arr = s.toCharArray();
         System.out.println(reverseOnlyLetters(arr));
 
@@ -13,14 +13,22 @@ public class reverseOnlyLetters {
         int j = s.length - 1;
         StringBuilder build = new StringBuilder();
 
-        while (i<j) {
-            while ((s[i]*1 <65 && s[i]*1 >90) || (s[i]*1 <97 && s[i]*1 >122)) {
+        while (i < j) {
+            while ((i < j) && (s[i]*1 < 65 || s[i]*1 > 90) && (s[i]*1 <97)) {
                 i++;
             }
-            while ((s[j]*1 <65 && s[j]*1 >90) || (s[j]*1 <97 && s[j]*1 >122)) {
-                j++;
+            while((j > i) && (s[j]*1 < 65 || s[j]*1 > 90) && (s[j]*1 <97)){
+                j--;
             }
-            swap(s,i,j);
+
+            if(i < s.length && j >= 0) {
+                swap(s,i,j);
+                i++;
+                j--;
+            }
+            else {
+                break;
+            }
         }
         for(char ch : s){
             build.append(ch);
