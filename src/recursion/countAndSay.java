3a23e81a0b7c78c5fd87cbdb6c2866939acc5794
om kaldate <omkaldate23@gmail.com>
2024-05-23 21:00:00 +0530

    ds

---
+++ src/recursion/countAndSay.java
@@ -0,0 +1,28 @@
+package recursion;
+
+public class countAndSay {
+    public static String countAndSay(int n) {
+        if(n==1){
+            return "1";
+        }
+        String say = countAndSay(n-1);
+
+        StringBuilder result = new StringBuilder();
+
+        for(int i = 0;i<say.length();i++){
+            char ch = say.charAt(i);
+            int count = 1;
+            while (i<say.length()-1 && say.charAt(i) == say.charAt(i+1)){
+                count++;
+                i++;
+            }
+            result.append(count).append(ch);
+        }
+        return  result.toString();
+    }
+
+
+    public static void main(String[] args){
+        System.out.println(countAndSay(4));
+    }
+}
