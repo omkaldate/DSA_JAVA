957aaf3141b8c4ff380783ce19c5c302de8f7a54
om kaldate <omkaldate23@gmail.com>
2024-07-28 15:54:11 +0530

    proble

---
+++ src/basicPrograms/ANumberAfterDoubleReversal.java
@@ -0,0 +1,11 @@
+package basicPrograms;
+
+public class ANumberAfterDoubleReversal {
+    public static void main(String[] args) {
+        int a = 123;
+        //int b = 1230;
+
+        if( a %10 ==0 && a != 0) System.out.println("true");
+        System.out.println("false");
+    }
+}
