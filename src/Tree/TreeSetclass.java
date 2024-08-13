d05d0584677fced6f88ad2eb11238f70414025be
om kaldate <omkaldate23@gmail.com>
2024-08-13 22:59:00 +0530

    Tree Binary Search

---
+++ src/Tree/TreeSetclass.java
@@ -0,0 +1,16 @@
+package Tree;
+import java.util.TreeSet;
+
+public class TreeSetclass {
+
+    public static void main(String[] args) {
+        TreeSet<Integer> st = new TreeSet<>();
+
+        st.add(40);
+        st.add(50);
+        st.add(10);
+
+
+        System.out.println(st);
+    }
+}
