4450629e45dcd9d462121b5e2e6948357ff07798
om kaldate <omkaldate23@gmail.com>
2024-08-22 22:46:12 +0530

    potd

--- src/HardProblems/SubstringwithConcatenationofAllWords.java
+++ src/HardProblems/SubstringwithConcatenationofAllWords.java
@@ -1,5 +1,45 @@
 package HardProblems;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class SubstringwithConcatenationofAllWords{
-    
+
+    public static List<Integer> findSubstring(String s, String[] words) {
+        List<Integer> ans = new ArrayList<>();
+        int len = words[0].length();
+        HashMap<String,Integer> mpp = new HashMap<>();
+
+        for(String word : words){
+            mpp.put(word, mpp.getOrDefault(word,0)+1);
+        }
+
+        for(int i=0; i<=s.length()-(words.length*len); i++){
+            HashMap<String,Integer> mpp1 = new HashMap<>(mpp);
+
+            for(int j=0; j<words.length; j++){
+                String str = s.substring(i+j*len , i+j*len+len);
+
+                if(mpp1.containsKey(str)){
+                    int cnt = mpp1.get(str);
+                    if(cnt == 1) mpp1.remove(str);
+                    else mpp1.put(str,cnt-1);
+
+                    if(mpp1.isEmpty()){
+                        ans.add(i);
+                    }
+                }
+                else break;
+            }
+        }
+        return ans;
+    }
+
+
+    public static void main(String[] args) {
+        String s = "wordgoodgoodgoodbestword";
+        String[] arr = {"word","good","best","good"};
+        System.out.println(findSubstring(s,arr));
+    }
 }
---
+++ src/bitMasking/NumberComolement.java
@@ -0,0 +1,15 @@
+package bitMasking;
+
+public class NumberComolement {
+    public static int findComplement(int num) {
+        if(num == 0) return 1;
+
+        int bitLength = Integer.toBinaryString(num).length();
+        int mask = (1 << bitLength) - 1;
+        return mask - num;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(findComplement(4));
+    }
+}
