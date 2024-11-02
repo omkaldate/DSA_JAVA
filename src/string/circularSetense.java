32b6b51cda6b91d87509cc5f8508140d1d4868b2
om kaldate <omkaldate23@gmail.com>
2024-11-02 21:44:21 +0530

    dsa

---
+++ src/string/circularSetense.java
@@ -0,0 +1,23 @@
+package string;
+
+public class circularSetense {
+    public boolean isCircularSentence(String sentence) {
+        String[] words = sentence.split(" ");
+        int n = words.length;
+
+        for (int i = 0; i < n; i++) {
+            String word1 = words[i];
+            String word2 = words[(i + 1) % n];
+            if (word1.charAt(word1.length() - 1) != word2.charAt(0)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+
+    public static void main(String[] args) {
+        
+    }
+}
