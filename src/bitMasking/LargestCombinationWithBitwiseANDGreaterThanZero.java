103031d9a116e720d2d381f3f553abe4e7383e73
om kaldate <omkaldate23@gmail.com>
2024-11-07 20:48:33 +0530

    dsa

---
+++ src/bitMasking/LargestCombinationWithBitwiseANDGreaterThanZero.java
@@ -0,0 +1,20 @@
+package bitMasking;
+
+public class LargestCombinationWithBitwiseANDGreaterThanZero {
+    public static int largestCombination(int[] candidates) {
+        int ans = 1;
+        for(int i=0; i<32; i++){
+            int c=0;
+            for(int x: candidates){
+                if((x&(1<<i))>0) c++;
+            }
+            ans = Math.max(ans, c);
+        }
+        return ans;
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+}
