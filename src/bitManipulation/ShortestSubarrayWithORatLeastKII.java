96282a264db91b0ba93b2d3ceff16fa37daca5cd
om kaldate <omkaldate23@gmail.com>
2024-11-10 20:30:50 +0530

    dsa

---
+++ src/bitManipulation/ShortestSubarrayWithORatLeastKII.java
@@ -0,0 +1,33 @@
+package bitManipulation;
+
+public class ShortestSubarrayWithORatLeastKII {
+    public static int minimumSubarrayLength(int[] nums, int k) {
+        if (k == 0) return 1;
+        int sum = 0;
+        int rs = Integer.MAX_VALUE;
+
+        for (int i = 0; i < nums.length; i++) {
+            sum |= nums[i];
+            if (sum >= k) {
+                int sum2 = 0;
+                int j = i;
+                while (sum2 < k) {
+                    sum2 |= nums[j--];
+                }
+                rs = Math.min(rs, i - j);
+                sum = 0;
+                i = j + 2;
+            }
+        }
+
+        if (rs == Integer.MAX_VALUE) {
+            return -1;
+        } else {
+            return rs;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
