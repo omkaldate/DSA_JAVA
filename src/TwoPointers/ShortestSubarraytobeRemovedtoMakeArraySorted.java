f275296d4388d64240a0155b80946820b2b671dc
om kaldate <omkaldate23@gmail.com>
2024-11-15 21:58:11 +0530

    dsa

---
+++ src/TwoPointers/ShortestSubarraytobeRemovedtoMakeArraySorted.java
@@ -0,0 +1,38 @@
+package TwoPointers;
+
+public class ShortestSubarraytobeRemovedtoMakeArraySorted {
+    public static int findLengthOfShortestSubarray(int[] arr) {
+        int n = arr.length;
+        int left = 0;
+        int right = n - 1;
+        int ans = 0;
+
+        while (right > 0 && arr[right - 1] <= arr[right]) {
+            right --;
+        }
+
+        if (right == 0) return 0;
+
+        while (left + 1 < n && arr[left] <= arr[left + 1]) {
+            left ++;
+        }
+
+        ans = Math.min(n - left - 1, right);;
+
+        int i = 0;
+        while (i <= left && right < n) {
+            if (arr[i] <= arr[right]) {
+                ans = Math.min (ans, right - i - 1);
+                i ++;
+            } else {
+                right ++;
+            }
+        }
+        return ans;
+    }
+
+
+    public static void main(String[] args) {
+        
+    }
+}
