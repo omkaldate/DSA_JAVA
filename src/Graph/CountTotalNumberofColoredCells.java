35b79be76c67bc03c30147190a72751913102824
om kaldate <omkaldate23@gmail.com>
2025-03-06 23:29:34 +0530

    dsa

---
+++ src/Graph/CountTotalNumberofColoredCells.java
@@ -0,0 +1,17 @@
+package Graph;
+
+import java.util.HashSet;
+
+public class CountTotalNumberofColoredCells {
+
+
+    public static void main(String[] args) {
+        HashSet<int[]> set = new HashSet<>();
+
+        set.add(new int[]{0,0});
+
+        set.add(new int[]{0,0});
+
+        System.out.println(set.size());
+    }
+}
